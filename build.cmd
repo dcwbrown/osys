65d9ebc4a136e60d80a07e00ef44281cf6f93064
David C W Brown <dave.github@brownsmeet.com>
2024-05-31 16:23:47 +0100

    Exe icon resource snapshot.

--- build.cmd
+++ build.cmd
@@ -1,88 +1,90 @@
-@echo ------------------------- Build Oberon compiler --------------------------
-@echo.
+@echo off
+echo ------------------------- Build Oberon compiler --------------------------
+echo.
 ::
 ::
-@rd /s /q build 2>NUL
-@md build >NUL 2>NUL
-@cd build
-@copy ..\knowngood\ORP.Compile.exe >NUL 2>NUL
-@copy ..\knowngood\Link.Link.exe >NUL 2>NUL
+rd /s /q build 2>NUL
+md build >NUL 2>NUL
+cd build
+copy ..\knowngood\ORP.Compile.exe >NUL 2>NUL
+copy ..\knowngood\Link.Link.exe >NUL 2>NUL
 ::
 ::
 ::  goto outercorebuild
-@fc ..\Link.Mod ..\knowngood\Link.Mod >NUL && goto noprebuild
+fc ..\Link.Mod ..\knowngood\Link.Mod >NUL && goto noprebuild
 :: goto noprebuild
 ::
 ::
-@echo.
-@echo ---------- Build prebuild linker with new link and old winhost -----------
-@echo.
+echo.
+echo ---------- Build prebuild linker with new link and old winhost -----------
+echo.
 ::
 ::
-@copy ..\knowngood\*.mod >NUL
-@copy /Y ..\Link.mod >NUL
-@ORP.Compile WinHost.Mod/s Kernel.Mod/s Files.Mod/s Modules.Mod/s ~ || goto end
-@ORP.Compile Fonts.Mod/s Texts.Mod/s Link.Mod/s ~                   || goto end
-@Link.Link Link                                                     || goto end
-@move /Y Link.exe Link.Link.exe >NUL
+copy ..\knowngood\*.mod >NUL
+copy /Y ..\Link.mod >NUL
+ORP.Compile WinHost.Mod/s Kernel.Mod/s Files.Mod/s Modules.Mod/s ~ || goto end
+ORP.Compile Fonts.Mod/s Texts.Mod/s Link.Mod/s ~                   || goto end
+Link.Link Link                                                     || goto end
+move /Y Link.exe Link.Link.exe >NUL
 ::
 ::
-@echo.
-@echo ----------- Build with known good compiler and updated linker ------------
-@echo.
 :noprebuild
+echo.
+echo ----------- Build with known good compiler and updated linker ------------
+echo.
 ::
-@copy /Y ..\*.mod >NUL
+copy /Y ..\*.mod >NUL
 ::
-@ORP.Compile WinHost.Mod/s Kernel.Mod/s Files.Mod/s Modules.Mod/s ~  || goto end
-@ORP.Compile Fonts.Mod/s Texts.Mod/s Link.Mod/s ORS.Mod/s ~          || goto end
-@ORP.Compile ORB.Mod/s X64.Mod/s Listing.Mod/s ORG.Mod/s ORP.Mod/s ~ || goto end
-@Link.Link ORP.Compile Link.Link                                     || goto end
-::
-::
-@echo.
-@echo --------------- Rebuild compiler with newly built compiler ---------------
-@echo.
+ORP.Compile WinHost.Mod/s Kernel.Mod/s Files.Mod/s Modules.Mod/s ~  || goto end
+ORP.Compile Fonts.Mod/s Texts.Mod/s Link.Mod/s ORS.Mod/s ~          || goto end
+ORP.Compile ORB.Mod/s X64.Mod/s Listing.Mod/s ORG.Mod/s ORP.Mod/s ~ || goto end
+Link.Link ORP.Compile Link.Link                                     || goto end
 ::
 ::
-@ORP.Compile WinHost.Mod/s Kernel.Mod/s Files.Mod/s Modules.Mod/s ~  || goto end
-@ORP.Compile Fonts.Mod/s Texts.Mod/s Link.Mod/s ORS.Mod/s ~          || goto end
-@ORP.Compile ORB.Mod/s X64.Mod/s Listing.Mod/s ORG.Mod/s ORP.Mod/s ~ || goto end
-@Link.Link ORP.Compile Link.Link                                     || goto end
+echo.
+echo --------------- Rebuild compiler with newly built compiler ---------------
+echo.
+::
+::
+ORP.Compile WinHost.Mod/s Kernel.Mod/s Files.Mod/s Modules.Mod/s ~  || goto end
+ORP.Compile Fonts.Mod/s Texts.Mod/s Link.Mod/s ORS.Mod/s ~          || goto end
+ORP.Compile ORB.Mod/s X64.Mod/s Listing.Mod/s ORG.Mod/s ORP.Mod/s ~ || goto end
+Link.Link ORP.Compile Link.Link                                     || goto end
 goto oberonbuild
 ::
 ::
-@echo.
-@echo --------------------------- Build outer core -----------------------------
-@echo.
+echo.
+echo --------------------------- Build outer core -----------------------------
+echo.
 :outercorebuild
 ::
-@copy /Y ..\*.mod >NUL
+copy /Y ..\*.mod >NUL
 ::
-@ORP.Compile WinHost.Mod/s Kernel.Mod/s Files.Mod/s Modules.Mod/s ~  || goto end
-@ORP.Compile Fonts.Mod/s Texts.Mod/s ~                               || goto end
+ORP.Compile WinHost.Mod/s Kernel.Mod/s Files.Mod/s Modules.Mod/s ~  || goto end
+ORP.Compile Fonts.Mod/s Texts.Mod/s ~                               || goto end
 ::
 ::
-@echo.
-@echo -------------------------- Build Oberon system ---------------------------
-@echo.
+echo.
+echo -------------------------- Build Oberon system ---------------------------
+echo.
 :oberonbuild
 ::
 ::
-@ORP.Compile FileDir.Mod/s WinGui.Mod/s Input.Mod/s Display.Mod/s ~ || goto end
-@ORP.Compile Viewers.Mod/s Oberon.Mod/s MenuViewers.Mod/s ~         || goto end
-@ORP.Compile TextFrames.Mod/s Edit.Mod/s System.Mod/s ~             || goto end
-@Link.Link Oberon                                                   || goto end
+ORP.Compile FileDir.Mod/s WinGui.Mod/s Input.Mod/s Display.Mod/s ~ || goto end
+ORP.Compile Viewers.Mod/s Oberon.Mod/s MenuViewers.Mod/s ~         || goto end
+ORP.Compile TextFrames.Mod/s Edit.Mod/s System.Mod/s ~             || goto end
+copy /Y ..\*.png >NUL 2>NUL
+Link.Link Oberon                                                   || goto end
 
-@echo.
-@echo --------------------------- Run Oberon system ----------------------------
-@echo.
+echo.
+echo --------------------------- Run Oberon system ----------------------------
+echo.
 ::
 ::
-@copy /Y ..\*.Tool >NUL 2>NUL
-@copy /Y ..\*.Fnt >NUL 2>NUL
+copy /Y ..\*.Tool >NUL 2>NUL
+copy /Y ..\*.Fnt >NUL 2>NUL
 Oberon
 ::
 ::
 :end
-@cd ..
+cd ..
